0079d364ef0d0cc0d9af089c10223093a0f0ed89
Andrew <andrew@egeler.us>
2020-12-05 15:22:34 -0500

    close unused fds the less broken but still crappy way

--- src/ps.cpp
+++ src/ps.cpp
@@ -8,7 +8,11 @@
 extern char **environ;
 
 namespace uea {
-    static int _setup_subprocess_fd(
+    struct _pipefds {
+        int read;
+        int write;
+    };
+    static _pipefds _setup_subprocess_fd(
         posix_spawn_file_actions_t &spawn_actions,
         subprocess::spawn_options options,
         int target_fd
@@ -19,7 +23,7 @@ namespace uea {
                             : throw "BOOM";
         if (type == subprocess::io::close) {
             posix_spawn_file_actions_addclose(&spawn_actions, target_fd);
-            return -1;
+            return {-1, -1};
         }
         else if (type == subprocess::io::open) {
             int p[2];
@@ -29,27 +33,39 @@ namespace uea {
                 posix_spawn_file_actions_adddup2(&spawn_actions, p[0], target_fd);
                 posix_spawn_file_actions_addclose(&spawn_actions, p[0]);
                 posix_spawn_file_actions_addclose(&spawn_actions, p[1]);
-                close(p[0]);
-                return p[1];
+                return {p[0], p[1]};
             }
             else {
                 posix_spawn_file_actions_adddup2(&spawn_actions, p[1], target_fd);
                 posix_spawn_file_actions_addclose(&spawn_actions, p[0]);
                 posix_spawn_file_actions_addclose(&spawn_actions, p[1]);
-                close(p[1]);
-                return p[0];
+                return {p[0], p[1]};
             }
         }
         else {
-            return -1;
+            return {-1, -1};
         }
     }
     subprocess::subprocess(std::vector<std::string> execute, spawn_options options) {
         posix_spawn_file_actions_t spawn_actions;
         posix_spawn_file_actions_init(&spawn_actions);
-        stdin = _setup_subprocess_fd(spawn_actions, options, 0);
-        stdout = _setup_subprocess_fd(spawn_actions, options, 1);
-        stderr = _setup_subprocess_fd(spawn_actions, options, 2);
+        std::vector<int> closefds;
+        _pipefds tmp;
+        tmp = _setup_subprocess_fd(spawn_actions, options, 0);
+        if(tmp.read >= 0) {
+            closefds.push_back(tmp.read);
+            stdin = tmp.write;
+        }
+        tmp = _setup_subprocess_fd(spawn_actions, options, 1);
+        if(tmp.read >= 0) {
+            closefds.push_back(tmp.write);
+            stdout = tmp.read;
+        }
+        tmp = _setup_subprocess_fd(spawn_actions, options, 2);
+        if(tmp.read >= 0) {
+            closefds.push_back(tmp.write);
+            stderr = tmp.read;
+        }
 
         std::vector<char*> args(execute.size() + 1);
         args[execute.size()] = 0;
@@ -63,6 +79,9 @@ namespace uea {
         int err = options.use_path ?
             posix_spawnp(&pid, args[0], &spawn_actions, nullptr, c_args, environ)
           : posix_spawn(&pid, args[0], &spawn_actions, nullptr, c_args, environ);
+
+        for (auto fd : closefds)
+            close(fd);
 
         if(err){throw "BOOM";}
     }
