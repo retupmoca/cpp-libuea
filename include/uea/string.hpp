4739c74d86c6ad793adab973c7991fc0f3e3ef58
Andrew <andrew@egeler.us>
2020-12-13 21:00:52 -0500

    add a secure compare

--- include/uea/string.hpp
+++ include/uea/string.hpp
@@ -4,4 +4,27 @@
 
 namespace uea {
     std::vector<std::string> split(char d, std::string line);
+
+    template<typename T>
+    bool secure_compare(T secret, T unknown) {
+        size_t ssize = secret.size();
+        size_t usize = unknown.size();
+
+        // get the corner cases out of the way. These aren't secure cases anyway,
+        // so don't bother going through the constant-time stuff
+        if (ssize==0 && usize==0)
+            return true;
+        if (ssize==0 || usize==0)
+            return false;
+
+        // assume whatever we're comparing fits in a register
+        // TODO: use c++ magic to get the actual subtype from T
+        uint64_t result = ssize ^ usize;
+
+        for (size_t i = 0; i < ssize; ++i) {
+            result |= secret[i] ^ unknown[i % usize];
+        }
+
+        return !result;
+    }
 }
